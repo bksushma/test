6ff31be36d9e69f5bd4925f7a19124d2536f56ae
bksushma <sushmavk2000@gmail.com>
2025-07-27 08:39:59 +0530

    Create test.sql
    test codes

---
+++ test.sql
@@ -0,0 +1,13 @@
+SELECT
+    COUNT(CASE WHEN IsAuthApproval = TRUE THEN 1 END) AS Pmt_Approval_TransactionId,
+    SUM(CASE WHEN IsAuthApproval = TRUE THEN AmountUSD END) AS Pmt_Approval_TransactionAmount,
+    COUNT(CASE WHEN IsAuthTerminalState = TRUE THEN 1 END) AS Pmt_Terminal_TransactionId,
+    SUM(CASE WHEN IsAuthTerminalState = TRUE THEN AmountUSD END) AS Pmt_Terminal_TransactionAmount
+FROM gold.transactions
+WHERE ProviderName <> 'Stored Value'
+  AND (CustomerOrMerchantInitiated = 'MerchantInitiated' OR IsPayNow = TRUE)
+  AND ConsumerOrCommercial = 'Commercial'
+  AND DunAttemptByCycle = 1
+  AND DynamicRetryAttempt < 1
+  AND DATE = '2025-07-01'
+;
