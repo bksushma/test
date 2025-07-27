c33ce767831466f2a9e146bb3c5b9172e61e2ce2
bksushma <sushmavk2000@gmail.com>
2025-07-27 11:48:12 +0530

    Update test.sql
    new

--- test.sql
+++ test.sql
@@ -1,13 +1,30 @@
 SELECT
-    COUNT(CASE WHEN IsAuthApproval = TRUE THEN 1 END) AS Pmt_Approval_TransactionId,
-    SUM(CASE WHEN IsAuthApproval = TRUE THEN AmountUSD END) AS Pmt_Approval_TransactionAmount,
-    COUNT(CASE WHEN IsAuthTerminalState = TRUE THEN 1 END) AS Pmt_Terminal_TransactionId,
-    SUM(CASE WHEN IsAuthTerminalState = TRUE THEN AmountUSD END) AS Pmt_Terminal_TransactionAmount
-FROM gold.transactions
-WHERE ProviderName <> 'Stored Value'
-  AND (CustomerOrMerchantInitiated = 'MerchantInitiated' OR IsPayNow = TRUE)
-  AND ConsumerOrCommercial = 'Commercial'
-  AND DunAttemptByCycle = 1
-  AND DynamicRetryAttempt < 1
-  AND DATE = '2025-07-01'
-;
+    -- Approval Percentage by Count
+    (
+        (SELECT Pmt_Approval_TransactionId FROM For_Dun_Commercial_FA) +
+        (SELECT Pmt_Approval_TransactionId FROM For_Dun_Consumer_FA) +
+        (SELECT Pmt_Approval_TransactionId FROM For_Pmt_Approval_CI_FA_NoPayNow) +
+        (SELECT Pmt_Approval_TransactionId FROM For_Pmt_Approval_MI_FA_NoDun)
+    ) * 100.0 /
+    NULLIF(
+        (SELECT Pmt_Terminal_TransactionId FROM For_Dun_Commercial_FA) +
+        (SELECT Pmt_Terminal_TransactionId FROM For_Dun_Consumer_FA) +
+        (SELECT Pmt_Terminal_TransactionId FROM For_Pmt_Approval_CI_FA_NoPayNow) +
+        (SELECT Pmt_Terminal_TransactionId FROM For_Pmt_Approval_MI_FA_NoDun),
+        0
+    ) AS Approval_Percentage_FA,
+
+    -- Approval Percentage by Amount (Dollar)
+    (
+        (SELECT Pmt_Approval_TransactionAmount FROM For_Dun_Commercial_FA) +
+        (SELECT Pmt_Approval_TransactionAmount FROM For_Dun_Consumer_FA) +
+        (SELECT Pmt_Approval_TransactionAmount FROM For_Pmt_Approval_CI_FA_NoPayNow) +
+        (SELECT Pmt_Approval_TransactionAmount FROM For_Pmt_Approval_MI_FA_NoDun)
+    ) * 100.0 /
+    NULLIF(
+        (SELECT Pmt_Terminal_TransactionAmount FROM For_Dun_Commercial_FA) +
+        (SELECT Pmt_Terminal_TransactionAmount FROM For_Dun_Consumer_FA) +
+        (SELECT Pmt_Terminal_TransactionAmount FROM For_Pmt_Approval_CI_FA_NoPayNow) +
+        (SELECT Pmt_Terminal_TransactionAmount FROM For_Pmt_Approval_MI_FA_NoDun),
+        0
+    ) AS Approval_PercentageDoller_FA
