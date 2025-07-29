4e6bdb8d4266813b66f2c07edead9b7e54ee1158
bksushma <sushmavk2000@gmail.com>
2025-07-29 21:48:46 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,10 +1,14 @@
-  FROM gold.transactions
-  WHERE
-    IsAuthTerminalState = TRUE
-    AND ProviderName <> 'Stored Value'
+SELECT
+    COUNT(CASE WHEN IsAuthApproval = TRUE THEN 1 END) AS Pmt_Approval_TransactionId,
+    SUM(CASE WHEN IsAuthApproval = TRUE THEN AmountUSD END) AS Pmt_Approval_TransactionAmount,
+    COUNT(CASE WHEN IsAuthTerminalState = TRUE THEN 1 END) AS Pmt_Terminal_TransactionId,
+    SUM(CASE WHEN IsAuthTerminalState = TRUE THEN AmountUSD END) AS Pmt_Terminal_TransactionAmount
+FROM gold.transactions
+WHERE 
+    ProviderName <> 'Stored Value'
     AND IsTransactionAbandoned = FALSE
     AND (CustomerOrMerchantInitiated = 'MerchantInitiated' OR IsPayNow = TRUE)
-    AND ConsumerOrCommercial = 'Consumer'
+    AND ConsumerOrCommercial = 'Commercial'
     AND IsLatestDunAttemptByCycle = TRUE
     AND (IsLastDynamicRetry = TRUE OR IsLastDynamicRetry IS NULL)
-)
+    AND DATE = '2025-07-01'
