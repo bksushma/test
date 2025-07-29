0a9343fc5c4e79a369b23a0dcf8d54a49e99bb90
bksushma <sushmavk2000@gmail.com>
2025-07-29 21:26:26 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,10 @@
-ok
+  FROM gold.transactions
+  WHERE
+    IsAuthTerminalState = TRUE
+    AND ProviderName <> 'Stored Value'
+    AND IsTransactionAbandoned = FALSE
+    AND (CustomerOrMerchantInitiated = 'MerchantInitiated' OR IsPayNow = TRUE)
+    AND ConsumerOrCommercial = 'Consumer'
+    AND IsLatestDunAttemptByCycle = TRUE
+    AND (IsLastDynamicRetry = TRUE OR IsLastDynamicRetry IS NULL)
+)
